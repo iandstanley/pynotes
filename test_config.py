82c096b334fdee5195dcbd8b7f9ee3081715b1a6
Ian Stanley <iandstanley@gmail.com>
2021-07-20 21:20:53 +0100

    added test for setting gpg key

--- test_config.py
+++ test_config.py
@@ -6,7 +6,7 @@ from pynotes import config, notesystem
 class TestConfig(unittest.TestCase):
     def test_init(self):
         n = notesystem()             # initialize notesystem as it will create NOTESDIR etc
-        
+ 
         c = config()
         self.assertTrue(c.initran)   # test __init__ runs to end
 
@@ -14,9 +14,7 @@ class TestConfig(unittest.TestCase):
         n = notesystem()             # initialize notesystem as it will create NOTESDIR etc
 
         c = config(git=True)
-        c.setGPGkey(n.getDefaultGPGkey())
-
-
+        c.setGPGkey(n.getDefaultGPGkey()) # fix up GPG key for tests (gets overwritten with second config() call
         c.writeConfig()             # write a configfile with usegit = True
         self.assertTrue(c.usegit)
 
@@ -28,15 +26,24 @@ class TestConfig(unittest.TestCase):
         n = notesystem()             # initialize notesystem as it will create NOTESDIR etc
 
         ng = config(git=False)      # setup without git
-        ng.setGPGkey(n.getDefaultGPGkey())
+        ng.setGPGkey(n.getDefaultGPGkey()) # fix up GPG key for tests (gets overwritten with second config() call
         ng.writeConfig()
         c = config()
         c.readConfig()
         self.assertFalse(c.usegit)
 
         wg = config(git=True)       # and without git
-        wg.setGPGkey(n.getDefaultGPGkey())
+        wg.setGPGkey(n.getDefaultGPGkey()) # fix up GPG key for tests (gets overwritten with second config() call
         wg.writeConfig()
         d = config()
         d.readConfig()
         self.assertTrue(d.usegit)
+
+    def test_setGPGkey(self):
+        n = notesystem()             # initialize notesystem as it will create NOTESDIR etc
+    
+        n.config.gpgkey="REMOVED"   # manually set gpg key
+        before = n.config.gpgkey
+        n.config.setGPGkey(n.getDefaultGPGkey())    # reset to first private key on keyring
+        after = n.config.gpgkey
+        self.assertNotEqual(before, after)        
